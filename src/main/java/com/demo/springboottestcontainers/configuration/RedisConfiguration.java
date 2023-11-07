7baa3ad5a4e98d6107e267638313f4d0f2614204
Ramazan Guner <ramazan.guner@haud.com>
2023-11-07 19:50:17 +0300

    redis json template

--- src/main/java/com/demo/springboottestcontainers/configuration/RedisConfiguration.java
+++ src/main/java/com/demo/springboottestcontainers/configuration/RedisConfiguration.java
@@ -1,18 +1,26 @@
 package com.demo.springboottestcontainers.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfiguration {
+
+    @Autowired
+    private RedisProperties redisProperties;
  
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisStandaloneConfiguration());
     }
  
     @Bean
@@ -20,6 +28,30 @@ public class RedisConfiguration {
         RedisTemplate<Long, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
         return template;
+    }
+
+    @Bean("jsonRedisTemplate")
+    public RedisTemplate<Long, Object> jsonRedisTemplate() {
+        RedisTemplate<Long, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(RedisSerializer.json());
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashValueSerializer(RedisSerializer.json());
+        template.setHashKeySerializer(RedisSerializer.json());
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+
+    private RedisStandaloneConfiguration redisStandaloneConfiguration() {
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration
+                = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+
+        if (StringUtils.hasText(redisProperties.getPassword())) {
+            redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        }
+        redisStandaloneConfiguration.setDatabase(redisProperties.getDatabase());
+
+        return redisStandaloneConfiguration;
     }
  
 }
--- src/main/java/com/demo/springboottestcontainers/service/ProductService.java
+++ src/main/java/com/demo/springboottestcontainers/service/ProductService.java
@@ -6,6 +6,8 @@ import com.demo.springboottestcontainers.exception.ResourceNotFoundException;
 import com.demo.springboottestcontainers.repository.CustomerRepository;
 import com.demo.springboottestcontainers.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +19,21 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    // to save product as json to redis
+    @Qualifier("jsonRedisTemplate")
+    private final RedisTemplate jsonRedisTemplate;
+
     public void deleteAll() {
         productRepository.deleteAll();
     }
 
     public Product save(Product product) {
         return productRepository.save(product);
+    }
+
+    public Product saveJson(Product product) {
+        jsonRedisTemplate.opsForValue().set(product.getId(), product);
+        return product;
     }
 
     public void saveAll(List<Product> products) {
