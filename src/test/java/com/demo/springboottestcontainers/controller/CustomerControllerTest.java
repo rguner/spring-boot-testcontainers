0573590e37b824fabae9d7518912f78f2e68c2f0
Ramazan Guner <ramazan.guner@haud.com>
2023-11-07 15:37:11 +0300

    rabbit mq listener added

--- src/test/java/com/demo/springboottestcontainers/controller/CustomerControllerTest.java
+++ src/test/java/com/demo/springboottestcontainers/controller/CustomerControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -55,6 +57,15 @@ class CustomerControllerTest {
     postgres.stop();
   }
   */
+
+  /* With @ServiceConnection annotation we can remove this @DynamicPropertySource annotated method
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+   */
 
 
   @Autowired
