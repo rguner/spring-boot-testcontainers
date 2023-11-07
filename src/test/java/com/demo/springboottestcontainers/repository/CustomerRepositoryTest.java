f2dd32670edd9b10f94b7e71d9c7b3b540bf0c21
Ramazan Guner <ramazan.guner@haud.com>
2023-11-07 11:29:09 +0300

    Repository test

---
+++ src/test/java/com/demo/springboottestcontainers/repository/CustomerRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.demo.springboottestcontainers.repository;
+
+import com.demo.springboottestcontainers.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+// do not replace the testcontainer data source
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+public class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:15-alpine"
+    );
+
+    @Test
+    public void testCustomerSaveAndFindById() {
+
+        Customer customer = Customer.builder().name("Ramazan Guner1").email("rguner1@gmail.com").build();
+        Customer savedCustomer = customerRepository.save(customer);
+
+        Optional<Customer> result = customerRepository.findById(savedCustomer.getId());
+        assertTrue(result.isPresent());
+
+        Customer customerFromGet = result.get();
+
+        assertEquals("Ramazan Guner1", customerFromGet.getName());
+        assertEquals("rguner1@gmail.com", customerFromGet.getEmail());
+
+    }
+
+    @Test
+    public void testCustomerCrud() {
+
+        Customer customer = Customer.builder().name("Ramazan Guner1").email("rguner1@gmail.com").build();
+        Customer savedCustomer = customerRepository.save(customer);
+
+        // read
+        Optional<Customer> result = customerRepository.findById(savedCustomer.getId());
+        assertTrue(result.isPresent());
+        Customer customerFromGet = result.get();
+        assertEquals("Ramazan Guner2", customerFromGet.getName());
+        assertEquals("rguner2@gmail.com", customerFromGet.getEmail());
+
+        // update
+        savedCustomer.setName("Rmazan Guner3");
+        savedCustomer.setEmail("rguner3@gmail.com");
+        customerRepository.save(savedCustomer);
+
+        // read by name
+        Optional<Customer> resultByName = customerRepository.findByName(savedCustomer.getName());
+        assertTrue(resultByName.isPresent());
+        Customer customerFromResultByName = result.get();
+        assertEquals("Ramazan Guner3", customerFromResultByName.getName());
+        assertEquals("rguner3@gmail.com", customerFromResultByName.getEmail());
+
+        // delete
+        customerRepository.delete(savedCustomer);
+        assertTrue(customerRepository.findById(savedCustomer.getId()).isEmpty());
+
+    }
+
+}
