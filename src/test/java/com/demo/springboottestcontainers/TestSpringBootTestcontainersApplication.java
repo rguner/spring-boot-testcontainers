a477a911af7ea2a13ad76a1ba88acefe67f96792
Ramazan Guner <ramazan.guner@haud.com>
2023-11-07 15:58:32 +0300

    rabbit mq bean added

--- src/test/java/com/demo/springboottestcontainers/TestSpringBootTestcontainersApplication.java
+++ src/test/java/com/demo/springboottestcontainers/TestSpringBootTestcontainersApplication.java
@@ -5,15 +5,27 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestSpringBootTestcontainersApplication {
 
+
+    /*
+        if there are these beans, SpringApplication connects to these created containers
+        else connects to the resources defined in application.yaml
+    */
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    @Bean
+    @ServiceConnection
+    static RabbitMQContainer rabbitMqContainer() {
+        return new RabbitMQContainer("rabbitmq:3.8.9");
     }
 
     public static void main(String[] args) {
