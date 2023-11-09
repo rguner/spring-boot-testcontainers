6af7ccfe1aace96d9afd12930f1d3dcb271427e2
Ramazan Guner <ramazan.guner@haud.com>
2023-11-09 10:38:48 +0300

    generic container

--- src/test/java/com/demo/springboottestcontainers/controller/ProductControllerTest.java
+++ src/test/java/com/demo/springboottestcontainers/controller/ProductControllerTest.java
@@ -13,6 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
@@ -29,9 +32,17 @@ class ProductControllerTest {
   @LocalServerPort
   private Integer port;
 
+
   @Container
   @ServiceConnection(name = "redis")
-  static GenericContainer redisContainer = new GenericContainer("redis:6.0.5");
+  static GenericContainer redisContainer = new GenericContainer("redis:6.0.5")
+          .withExposedPorts(6379);
+
+  @DynamicPropertySource //  //Couldnt gget host and port from container with @ServiceConnection annotation for GenericContainer
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.redis.host", redisContainer::getHost);
+    registry.add("spring.data.redis.port", redisContainer::getFirstMappedPort);
+  }
 
   @Container
   @ServiceConnection
